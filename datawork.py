d0dfdf93669159122b3681a0b06c97af02e8a854
Lindsay Bernath <78574116+lybrn@users.noreply.github.com>
2025-09-25 03:29:31 +0000

    wip

--- datawork.py
+++ datawork.py
@@ -1,5 +1,12 @@
 import pandas as pd 
-dfs = pd.read_html("https://docs.google.com/document/d/e/2PACX-1vTMOmshQe8YvaRXi6gEPKKlsC6UpFJSMAk4mQjLm_u1gmHdVVTaeh7nBNFBRlui0sTZ-snGwZM4DBCT/pub", encoding='utf-8')
+url1 = "https://docs.google.com/document/d/e/2PACX-1vTMOmshQe8YvaRXi6gEPKKlsC6UpFJSMAk4mQjLm_u1gmHdVVTaeh7nBNFBRlui0sTZ-snGwZM4DBCT/pub";
+url2 = "https://docs.google.com/document/d/e/2PACX-1vRPzbNQcx5UriHSbZ-9vmsTow_R6RRe7eyAU60xIF9Dlz-vaHiHNO2TKgDi7jy4ZpTpNqM7EvEcfr_p/pub"
+dfs = pd.read_html(url2, encoding='utf-8')
+output = []
 for df in dfs:
     for index, row in df.iterrows():
-        print(f"Index: {index}, Row: {row[0]} {row[1]} {row[2]}")
+       # x = row[0]
+       # char = row[1]
+       # y = row[2]
+       # print char,
+       print(f"Index: {index}, Row: {row[0]} {row[1]} {row[2]}")
